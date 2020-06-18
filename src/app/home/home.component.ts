3a985789dea269321d62a40996907b693c798e7e
gio199 <50559035+gio199@users.noreply.github.com>
2020-06-18 19:49:48 +0400

    Add files via upload

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   clickCounter: number = 0;
-  name: string = 'Baba Cool';
+  name: string = 'This is the last bottle';
 
   constructor() { }
 
